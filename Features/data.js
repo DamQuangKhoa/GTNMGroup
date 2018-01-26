ee6391a6b9ac2d8f52295ff2c21e73e3531c09aa
Thế Nguyễn Mai <thenguyen.mai@gmail.com>
2018-01-26 17:47:33 +0700

    features

--- Features/data.js
+++ Features/data.js
@@ -1,7 +1,7 @@
 var data = [
     {
         "id": "dangnhap",
-        "req_content": "Trang đăng nhập",
+        "req_content": "<p>Trang đăng nhập</p>",
         "des_img": [],
         "des_url": "",
         "impl_iframe": ""
@@ -9,7 +9,7 @@ var data = [
 
     {
         "id": "dangbai",
-        "req_content": "Trang đăng bài",
+        "req_content": "<p>Trang đăng bài</p>",
         "des_img": ["img/posting_screen.png"],
         "des_url": "epgz/posting.epgz",
         "impl_iframe": "../ProjectFrontEnd/posting/posting.html"
@@ -17,7 +17,7 @@ var data = [
 
     {
         "id": "newfeed",
-        "req_content": "Trang Newfeed",
+        "req_content": "<p>Trang Newfeed</p>",
         "des_img": [],
         "des_url": "",
         "impl_iframe": "4"
@@ -25,7 +25,7 @@ var data = [
 
     {
         "id": "trangcanhan",
-        "req_content": "Trang cá nhân",
+        "req_content": "<p>Trang cá nhân</p>",
         "des_img": [],
         "des_url": "",
         "impl_iframe": ""
@@ -33,10 +33,66 @@ var data = [
 
     {
         "id": "doithuong",
-        "req_content": "Trang đổi thưởng",
-        "des_img": ["img/gift_screen.jpg"],
-        "des_url": "",
+        "req_content": "<p>Trang đổi thưởng</p>",
+        "des_img": ["img/doithuong_screen.jpg", "img/doithuong1_screen.jpg"],
+        "des_url": "epgz/doithuong.zip",
         "impl_iframe": "../ProjectFrontEnd/redeemingreward/index.html"
+    },
+
+    {
+        "id": "tintuc1",
+        "req_content": "<p>Trang tin tức 1</p>",
+        "des_img": ["../Chucnang/images/tintuc1.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../ProjectFrontEnd/TrangTinTuc.html"
+    },
+
+    {
+        "id": "tintuc2",
+        "req_content": "<p>Trang tin tức 2</p>",
+        "des_img": ["../Chucnang/images/tintuc2.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../ProjectFrontEnd/TrangTinTuca.html"
+    },
+
+    {
+        "id": "phancong",
+        "req_content": "<p>Phân công</p>",
+        "des_img": ["../Chucnang/images/phancong.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../ProjectFrontEnd/groupdiary/group.html"
+    },
+
+    {
+        "id": "trangchu",
+        "req_content": "<p>Trang chủ</p>",
+        "des_img": ["../Chucnang/images/index.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../ProjectFrontEnd/index.html"
+    },
+
+    {
+        "id": "admin",
+        "req_content": "<p>Trang Admin</p>",
+        "des_img": ["../Chucnang/images/trangadmin.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../Admin/Admin.html"
+    },
+
+    {
+        "id": "quanlybaidang",
+        "req_content": "<p>Trang quản lí bài đăng</p>",
+        "des_img": ["../Chucnang/images/quanlybaidang.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../Admin/quanlibaidang.html"
+    },
+
+    {
+        "id": "quanlynguoidung",
+        "req_content": "<p>Trang quản lí người dùng</p>",
+        "des_img": ["../Chucnang/images/quanlynguoidung.png"],
+        "des_url": "https://drive.google.com/drive/u/1/folders/1uttdZeYBu1Oeq0HrqZyqigFHpkXcLtaU",
+        "impl_iframe": "../Admin/quanlinguoidung.html"
     }
 
 
