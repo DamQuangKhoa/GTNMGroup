8d8a7aba484e731d80712030ab026703831a76ce
Thế Nguyễn Mai <thenguyen.mai@gmail.com>
2018-01-26 16:03:25 +0700

    features

---
+++ Features/data.js
@@ -0,0 +1,43 @@
+var data = [
+    {
+        "id": "dangnhap",
+        "req_content": "1",
+        "des_img": "",
+        "des_url": "",
+        "impl_iframe": ""
+    },
+
+    {
+        "id": "dangbai",
+        "req_content": "2",
+        "des_img": "img/posting_screen.png",
+        "des_url": "epgz/posting.epgz",
+        "impl_iframe": "../ProjectFrontEnd/posting/posting.html"
+    },
+
+    {
+        "id": "newfeed",
+        "req_content": "3",
+        "des_img": "",
+        "des_url": "",
+        "impl_iframe": "4"
+    },
+
+    {
+        "id": "trangcanhan",
+        "req_content": "5",
+        "des_img": "",
+        "des_url": "",
+        "impl_iframe": ""
+    },
+
+    {
+        "id": "doithuong",
+        "req_content": "6",
+        "des_img": "",
+        "des_url": "",
+        "impl_iframe": ""
+    }
+
+
+];
