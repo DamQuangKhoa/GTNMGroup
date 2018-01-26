75c3b51409f835162d9a651d59cc08f616586656
Thế Nguyễn Mai <thenguyen.mai@gmail.com>
2018-01-26 16:51:49 +0700

    features

--- Features/data.js
+++ Features/data.js
@@ -1,42 +1,42 @@
 var data = [
     {
         "id": "dangnhap",
-        "req_content": "1",
-        "des_img": "",
+        "req_content": "Trang đăng nhập",
+        "des_img": [],
         "des_url": "",
         "impl_iframe": ""
     },
 
     {
         "id": "dangbai",
-        "req_content": "2",
-        "des_img": "img/posting_screen.png",
+        "req_content": "Trang đăng bài",
+        "des_img": ["img/posting_screen.png"],
         "des_url": "epgz/posting.epgz",
         "impl_iframe": "../ProjectFrontEnd/posting/posting.html"
     },
 
     {
         "id": "newfeed",
-        "req_content": "3",
-        "des_img": "",
+        "req_content": "Trang Newfeed",
+        "des_img": [],
         "des_url": "",
         "impl_iframe": "4"
     },
 
     {
         "id": "trangcanhan",
-        "req_content": "5",
-        "des_img": "",
+        "req_content": "Trang cá nhân",
+        "des_img": [],
         "des_url": "",
         "impl_iframe": ""
     },
 
     {
         "id": "doithuong",
-        "req_content": "6",
-        "des_img": "",
+        "req_content": "Trang đổi thưởng",
+        "des_img": ["img/gift_screen.jpg"],
         "des_url": "",
-        "impl_iframe": ""
+        "impl_iframe": "../ProjectFrontEnd/redeemingreward/index.html"
     }
 
 
